1d4dfdfa9a15109127ad494e483374782cf5e1b3
Nindy Odyani <73346751+nindyodyanii@users.noreply.github.com>
2024-12-24 20:56:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 130);
+});
+
+let menu = document.querySelector('#menu-icon');
+let menulist = document.querySelector('.menulist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    menulist.classList.toggle('open');
+}
